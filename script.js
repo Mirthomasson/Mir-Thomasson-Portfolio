e63d1b94ca2de364600b642f8cb447288eabb748
MirThomasson <93678569+Mirthomasson@users.noreply.github.com>
2022-05-25 18:27:52 -0400

    contact redo

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-$('a.thumb1').on('click',function(){ // when press link
 
-    var fullImg = $(this).attr('href') // get full size img url
-    $('#popup img').attr('src', fullImg) // image in popup now has source that you need
-    $('#popup').show() // popup appears
-    return false // do not redirect 
- 
- })
- 
- $('.close_popup').on('click', function(){
- 
-    $('#popup').hide() // close popup
- 
- })
 
 
 // const object = document.getElementById('pulsate');
