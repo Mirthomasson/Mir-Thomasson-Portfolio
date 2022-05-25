2c0a30888b9d0f29ec7f6cf59d2f0c0ea38624bb
MirThomasson <93678569+Mirthomasson@users.noreply.github.com>
2022-05-25 17:09:22 -0400

    contatct page

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
+$('a.thumb1').on('click',function(){ // when press link
+
+    var fullImg = $(this).attr('href') // get full size img url
+    $('#popup img').attr('src', fullImg) // image in popup now has source that you need
+    $('#popup').show() // popup appears
+    return false // do not redirect 
+ 
+ })
+ 
+ $('.close_popup').on('click', function(){
+ 
+    $('#popup').hide() // close popup
+ 
+ })
 
 
+// const object = document.getElementById('pulsate');
 
-const object = document.getElementById('pulsate');
-
-function logger() {
+// function logger() {
   
-}
+// }
 
-object.addEventListener('click', logger);
-object.removeEventListener('click', logger);
+// object.addEventListener('click', logger);
+// object.removeEventListener('click', logger);
