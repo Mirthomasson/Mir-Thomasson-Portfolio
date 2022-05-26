b8c10bf553f152d30b41e01f65fa0782024e0886
MirThomasson <93678569+Mirthomasson@users.noreply.github.com>
2022-05-25 20:16:30 -0400

    new home page

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 
-
-
-// const object = document.getElementById('pulsate');
-
-// function logger() {
-  
-// }
-
-// object.addEventListener('click', logger);
-// object.removeEventListener('click', logger);
+const gra = function(min, max) {
+    return Math.random() * (max - min) + min;
+}
+const init = function(){
+  let items = document.querySelectorAll('section');
+  for (let i = 0; i < items.length; i++){
+    items[i].style.background = randomColor({luminosity: 'light'});
+  }
+  cssScrollSnapPolyfill()
+}
+init();
