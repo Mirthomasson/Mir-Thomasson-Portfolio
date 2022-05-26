514fb8593b30464a1a54a447eaea992fed36349c
MirThomasson <93678569+Mirthomasson@users.noreply.github.com>
2022-05-26 14:09:11 -0400

    profile pic generator

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+/* ----------profile pic---------- */
+
+function changeImage() {
+  var image = document.getElementById('myImage');
+  if (image.src.match("contact")) {
+      image.src = "./images/rainbow-mir.png";
+  } else if (image.src.match("rainbow")) {
+      image.src = "./images/trippy-mir.png";
+  } else if (image.src.match("trippy")) {
+      image.src = "./images/tiramasu-mir.png";
+  } else {
+      image.src = "./images/contact-pic.png";
+  }
+}
+/* ----------stars---------- */
 
 "use strict";
 
